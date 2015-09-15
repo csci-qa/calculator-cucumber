31d455cfb08ef18ce79e642716d9826ad13bf99e
Suke Hozumi <yusuke.hozumi@icloud.com>
2015-09-14 22:22:51 -0400

    added test for dividing same numbers in different order

--- features/division.feature
+++ features/division.feature
@@ -3,4 +3,11 @@ Feature: Division
   I want my calculator to divide numbers
   So i can know their quotient
 
+  Scenario: Dividing numbers to get a quotient
+    When I divide two numbers
+    Then I get the quotient
+
+  Scenario: Dividing numbers in different order
+    When I divide the same numbers in different orders
+    Then the quotients are not the same
 
--- features/step_definitions/division_steps.rb
+++ features/step_definitions/division_steps.rb
@@ -1,3 +1,20 @@
 require 'Calculator'
 
+When /^I divide two numbers$/ do
+  @quotient = Calculator.new.divide 10, 2
+end
+
+Then /^I get the quotient$/ do
+  expect(@quotient).to eq 5
+end
+
+When /^I divide the same numbers in different orders$/ do
+  calc = Calculator.new
+  @quotient1 = calc.divide 10, 2
+  @quotient2 = calc.divide 2, 10
+end
+
+Then /^the quotients are not the same$/ do
+  expect(@quotient1).to_not eq @quotient2
+end
 
