619a39eff909027ab0fbb677823ead2158564e6d
Suke Hozumi <yusuke.hozumi@icloud.com>
2015-09-14 20:54:04 -0400

    Copied methods over from other calculator.rb file

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,16 @@ class Calculator
   def add *numbers
     numbers.inject(:+)
   end
+  
+  def subtract *numbers
+  	numbers.inject(:-)
+  end
+  
+  def multiply *numbers
+  	numbers.inject(:*)
+  end
+  
+  def divide *numbers
+  	numbers.inject(:/)
+   end 
 end
