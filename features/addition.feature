e02eedc115d6fb71c8f0b0a5c6eb500041a1fcb7
Joseph E Vergara <jvergara3086@gmail.com>
2015-09-16 15:30:15 -0400

    Cucumber wasn't working so committed everything at once   Couldn't test code so I did everything in Notepad++ and   committed it all at one time.

--- features/addition.feature
+++ features/addition.feature
@@ -18,3 +18,70 @@ Feature: Addition
   Scenario: Adding more than two numbers
     When I add more than two numbers
     Then I get the sum
+	
+
+  Scenario: Subtracting numbers to get the difference
+    When I subtract two numbers
+	Then I get the difference
+
+  Scenario: Subtracting numbers in different orders
+    When I subtract the same numbers in different orders
+	Then the difference is not the same
+
+  Scenario: Subtracting zero
+    When I subtract zero from a number
+	Then the difference is that number
+
+  Scenario: Subtracting two identical numbers
+    When I subtract to identical numbers
+	Then the difference is zero
+
+  Scenario: Subtracting more than two numbers
+    When I subtract more than two numbers
+	Then I get the difference
+
+
+  Scenario: Multiplying numbers to get the product
+    When I multiply two numbers
+	Then I get the product
+
+  Scenario: Multiplying numbers in different orders
+    When I multiply the same numbers in different orders
+	Then the product is the same
+
+  Scenario: Multiplying by one
+    When I multiply a number by one
+	Then the product should be the first number
+
+  Scenario: Multiplying by zero
+    When I multiply a number by zero
+	Then the product should be zero
+
+  Scenario: Multiplying more than two numbers
+    When I multiply more than two numbers
+	Then I get the product
+
+
+  Scenario: Dividing numbers to get the quotient
+    When I divide two numbers
+	Then I get the quotient
+
+  Scenario: Dividing numbers in different orders
+    When I divide the same numbers in different orders
+	Then the quotient is different
+
+  Scenario: Dividing by one
+    When I divide by one
+	Then the quotient should be the first number
+
+  Scenario: Dividing zero by another number
+    When I divide zero by another number
+	Then the quotient should be zero
+
+  Scenario: Dividing by zero
+    When I divide by zero
+	Then an error should be raised
+
+  Scenario: Dividing by more than two numbers
+    When I divide by more than two numbers
+	Then I get the quotient
--- features/step_definitions/addition_steps.rb
+++ features/step_definitions/addition_steps.rb
@@ -29,3 +29,128 @@ end
 When /^I add more than two numbers$/ do
   @sum = Calculator.new.add 15, 6, 8, 0
 end
+
+
+When /^I subtract two numbers$/ do
+  @difference = Calculator.new.subtract 13,3
+end
+
+Then /^I get the difference$/ do
+  expect(@difference).to eq 10
+end
+
+When /^I subtract the same numbers in different orders$/ do
+  calc = Calculator.new
+  @difference1 = calc.subtract 4,6
+  @difference2 = calc.subtract 6,4
+end
+
+Then /^the difference is not the same$/ do
+  expect(@difference1).not_to eq @difference2
+end
+
+When /^I subtract zero from a number$/ do
+  @difference = Calculator.new.subtract 7,0
+end
+
+Then /^the difference is that number$/ do
+  expect(@difference).to eq 7
+end
+
+When /^I subtract to identical numbers$/ do
+  @difference = Calculator.new.subtract 5,5
+end
+
+Then /^the difference is zero$/ do
+  expect(@difference).to eq 0
+end
+
+When /^I subtract more than two numbers$/ do
+  @difference = Calculator.new.subtract 13,2,1
+end
+
+
+When /^I multiply two numbers$/ do
+  @product = Calculator.new.multiply 6,2
+end
+
+Then /^I get the product$/ do
+  expect(@product).to eq 12
+end
+
+When /^I multiply the same numbers in different orders$/ do
+  calc = Calculator.new
+  @product1 = calc.multiply 3,7
+  @product2 = calc.multiply 7,3
+end
+
+Then /^the product is the same$/ do
+  expect(@product1).to eq @product2
+end
+
+When /^I multiply a number by one$/ do
+  @product = Calculator.new.multiply 5,1
+end
+
+Then /^the product should be the first number$/ do
+  expect(@product).to eq 5
+end
+
+When /^I multiply a number by zero$/ do
+  @product = Calculator.new.multiply 8,0
+end
+
+Then /^the product should be zero$/ do
+  expect(@product).to eq 0
+end
+
+When /^I multiply more than two numbers$/ do
+  @product = Calculator.new.multiply 3,2,2
+end
+
+
+When /^I divide two numbers$/ do
+  @quotient = Calculator.new.divide 12,6
+end
+
+Then /^I get the quotient$/ do
+  expect(@quotient).to eq 2
+end
+
+When /^I divide the same numbers in different orders$/ do
+  calc = Calculator.new
+  @quotient1 = calc.divide 6,3
+  @quotient2 = calc.divide 3,6
+end
+
+Then /^the quotient is different$/ do
+  expect(@quotient1).not_to eq @quotient2
+end
+
+When /^I divide by one$/ do
+  @quotient = Calculator.new.divide 4,1
+end
+
+Then /^the quotient should be the first number$/ do
+  expect(@quotient).to eq 4
+end
+
+When /^I divide zero by another number$/ do
+  @quotient = Calculator.new.divide 0,3
+end
+
+Then /^the quotient should be zero$/ do
+  expect(@quotient).to eq 0
+end
+
+When /^I divide by zero$/ do
+  @quotient = Calculator.new.divide 5,0
+end
+
+Then /^an error should be raised$/ do
+  expect(@quotient).to raise_error
+end
+
+When /^ I divide by more than two numbers$/ do
+  @quotient = Calculator.new.divide 12,2,3
+end
