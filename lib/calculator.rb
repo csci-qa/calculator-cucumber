c46978a9daac0c335f6930469d7e87566dc27d7d
Scott Felske <sbfelske@gmail.com>
2015-11-08 18:29:44 -0500

    calculator cucumber update

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,17 @@ class Calculator
   def add *numbers
     numbers.inject(:+)
   end
+  
+  def subtract *numbers
+    numbers.inject(:-)
+  end
+  
+  def multiply *numbers
+	numbers.inject(:*)
+  end
+  
+  def division *numbers
+    numbers.inject(:/)
+  end
+  
 end
